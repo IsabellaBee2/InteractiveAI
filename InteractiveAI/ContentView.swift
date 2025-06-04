250d93aac3fb5619067d5b53e68529861ef45718
Isabella <ibarron083@rsdmo.org>
2025-06-04 15:58:49 -0500

    text box

--- InteractiveAI/ContentView.swift
+++ InteractiveAI/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    var name = ""
+    
     var body: some View {
-      
-    } //end of body
+        VStack {
+            Text("What is your name?")
+                .font(.title)
+            TextField("type you name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("here") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
+        .padding()
+    }
 }
 //end of struct
 #Preview {
