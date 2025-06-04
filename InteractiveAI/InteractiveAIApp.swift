963309ca43b4bcc42d3a3b2a1e7bd61f471776d8
Isabella <ibarron083@rsdmo.org>
2025-06-04 14:16:08 -0500

    Initial Commit

---
+++ InteractiveAI/InteractiveAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveAIApp.swift
+//  InteractiveAI
+//
+//  Created by Scholar on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InteractiveAITests/InteractiveAITests.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveAITests.swift
+//  InteractiveAITests
+//
+//  Created by Scholar on 6/4/25.
+//
+
+import Testing
+@testable import InteractiveAI
+
+struct InteractiveAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
